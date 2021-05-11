cb2218c6896a4ef75cc79e14c6750f757ec1e600
lolguineapig <68605689+lolguineapig@users.noreply.github.com>
2021-05-11 12:20:18 -0400

    Music Toggle

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-music = new Audio('bgmusic.mp3')
-music.play()
-
-alert("Website still in progress. Click 'Okay' if understood.")
+  var musicKeyPress = event.keyCode;
+  var music = new Audio("pokepMusic.mp3")
+  if (x == 77) {
+    var playMusic = confirm("Would you like some music to be played?")
+    if (playMusic == true) {
+      music.play()
+    }
+    else if (playMusic == false) {
+    }
+  }
