17f6939bcf94658e6cf829d056b29fc88dc25cfc
da SEA Turtle <76405735+alienloverA9@users.noreply.github.com>
2021-07-06 13:47:15 -0400

    changing some stuff
    important stufff

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
   var musicKeyPress = event.keyCode;
   var music = new Audio("pokepMusic.mp3")
-  if (x == 77) {
+  if (musicKeyPress == 77) {
     var playMusic = confirm("Would you like some music to be played?")
     if (playMusic == true) {
       music.play()
